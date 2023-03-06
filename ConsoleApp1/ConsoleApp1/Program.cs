d9c4e84efe69cb0aafeb68d0babc71e6e2fabf4a
Giedrasdangus <126971598+Giedrasdangus@users.noreply.github.com>
2023-03-06 22:39:44 +0200

    Mano pirmas bandymas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> Bendradarbiai = new List<string>(4);
+
+            Bendradarbiai.Add("Andrius");
+            Bendradarbiai.Add("Edvinas");
+            Bendradarbiai.Add( "Justas");
+            Bendradarbiai.Add ("Darius");
+  
+            foreach(string i in Bendradarbiai)
+            {
+                Console.WriteLine("bendradarbis {0}", i);
+            }
+        }
+    }
+}
